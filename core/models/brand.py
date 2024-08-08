88ad26979a3c580b0443a8c0db26d864479f22de
Wecreds <Wecredsbr@gmail.com>
2024-08-08 07:29:41 -0300

    feat: Adicionando "blank=True" no atributo "Nationality" de "Brand"

--- core/models/brand.py
+++ core/models/brand.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Brand(models.Model):     
     name = models.CharField(max_length=100)     
-    nationality = models.CharField(max_length=50, null=True)
+    nationality = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):         
         return self.name.upper()          
